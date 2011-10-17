b793f1182a84244ce848a72af2c07c76a2cd2734
George Cairns <georgecairns@hbosplc.com>
2011-10-17 16:08:26 +0100

    fully working, publishable, including collector.

---
+++ WebApplication3/WebApplication3/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string v = Request.QueryString["startdate"];
+            if (v == null)
+            {
+                // Get 5PM on the last weekday
+                DateTime StartDate;
+                switch (DateTime.Today.DayOfWeek) {
+                    case DayOfWeek.Monday:
+                        StartDate = DateTime.Now.Date.AddHours(-72 + 17);
+                        break;
+                    case DayOfWeek.Sunday:
+                        StartDate = DateTime.Now.Date.AddHours(-48 + 17);
+                        break;
+                    default:
+                        StartDate = DateTime.Now.Date.AddHours(-24 + 17);
+                        break;
+                }
+
+                Response.Redirect("/?startdate=" + StartDate.ToString());
+             
+            }
+        }
+    }
+}
