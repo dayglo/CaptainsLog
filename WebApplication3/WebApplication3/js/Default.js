0fb7811dddf6eaec4cf4242119cacf5ed9b55a58
George Cairns <george.cairns@lloydsbanking.com>
2012-01-31 13:04:50 +0000

    driven from json object, added working implementation of datatables

--- WebApplication3/WebApplication3/js/Default.js
+++ WebApplication3/WebApplication3/js/Default.js
@@ -1,107 +1,123 @@
-﻿
+﻿function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
 
 
-$(function () {
+
+
+
+//var startDate = new Date();
+//var endDate = new Date();
+
+
+$(document).ready(function () {
+
+    //If there is no startdate or enddate, set them to be the period since the last working day
+    startDate = getUrlVars()["startDate"];
+    endDate = getUrlVars()["endDate"];
+    if (startDate == undefined) { startDate = "1/14/2012 10:00" }
+    if (endDate == undefined) { endDate = new Date().toString() }
+
+    //click handler for table rows
+    $('tr').click(function () {
+        alert("ed");
+        if ($(this).hasClass('row_selected'))
+            $(this).removeClass('row_selected');
+        else
+            $(this).addClass('row_selected');
+    });
 
     var ReportsToRequest = {
         "ReportingAreas": [
             {
-                "Area":"Heritage HBOS",
+                "Name": "Heritage HBOS",
                 "Environments": [
-                    {"name":"Infra","VCServer":"infrp0101"},
-                    {"name":"Preprod","VCServer":"infrp0100"},
-                    {"name":"Prod","VCServer":"infrl0100"}
+                    { "name": "Infra", "VCServer": "infrp0101" },
+                    { "name": "Preprod", "VCServer": "infrp0100" },
+                    { "name": "Prod", "VCServer": "infrl0100" }
                 ]
             },
             {
-                "Area":"Distributed Platform Readiness (DPR)",
+                "Name": "Distributed Platform Readiness (DPR)",
                 "Environments": [
-                    {"name":"Preprod - PB2","VCServer":"preprodpb2"},
-                    {"name":"Preprod - PB3","VCServer":"preprodpb3"},
-                    {"name":"Live - PB2","VCServer": "prodpb2"},
-                    {"name":"Live - PB3","VCServer": "prodpb3"}
+                    { "name": "Preprod - PB2", "VCServer": "preprodpb2" },
+                    { "name": "Preprod - PB3", "VCServer": "preprodpb3" },
+                    { "name": "Live - PB2", "VCServer": "prodpb2" },
+                    { "name": "Live - PB3", "VCServer": "prodpb3" }
                 ]
             },
             {
-                "Area":"Strategic Development Infrastructure (SDI)",
+                "Name": "Strategic Development Infrastructure (SDI)",
                 "Environments": [
-                    {"name":"Preprod","VCServer":"preprodsdipb2"},
-                    {"name":"Live - PB2 #1","VCServer":"prodsdipb2_1"},
-                    {"name":"Live - PB2 #2","VCServer":"prodsdipb2_2"},
-                    {"name":"Live - PB3 #1","VCServer":"prodsdipb3_1"},
-                    {"name":"Live - PB3 #2","VCServer":"prodsdipb3_2"}
+                    { "name": "Preprod", "VCServer": "preprodsdipb2" },
+                    { "name": "Live - PB2 #1", "VCServer": "prodsdipb2_1" },
+                    { "name": "Live - PB2 #2", "VCServer": "prodsdipb2_2" },
+                    { "name": "Live - PB3 #1", "VCServer": "prodsdipb3_1" },
+                    { "name": "Live - PB3 #2", "VCServer": "prodsdipb3_2" }
                 ]
             },
             {
-                "Area":"Distributed Services Pre-Provisioning (DSPP)",
+                "Name": "Distributed Services Pre-Provisioning (DSPP)",
                 "Environments": [
-                    {"name":"Preprod","VCServer":"proddspp"},
-                    {"name":"Live","VCServer":"e2eassurance"}
+                    { "name": "Preprod", "VCServer": "proddspp" },
+                    { "name": "Live", "VCServer": "e2eassurance" }
                 ]
             }
         ]
     }
 
 
-    var environments = ["infrp0101", "infrp0100", "infrl0100", "preprodpb2", "preprodpb3", "prodpb2", "prodpb3", "preprodsdipb2", "prodsdipb2_1", "prodsdipb2_2", "prodsdipb3_1", "prodsdipb3_2", "preproddspp", "proddspp", "e2eassurance"];
-    var startDate = "1/14/2012 10:00";
-    var endDate = "1/15/2012 11:00";
 
 
-    $('#Button1').click(function(){
+
+    $('#Button1').click(function () {
+
+
+
+        // Populate everything
+        //====================
+
+
         $('#output').empty();
-        
+        $('#output').append('<h1>VMware Health Check</h2>');
         $.each(
-            environments,
-            function (intIndex, objValue) {
-                $('#output').append('<table id="table_'+ objValue +'"></table><div id="pager_'+ objValue +'"></div>');
-                getPosts(objValue, startDate, endDate);
-            }
+        //For each reporting area
+            ReportsToRequest.ReportingAreas,
+            function (i, area) {
+                //...write the area name
+                $('#output').append('<h2>' + area.Name + '</h2>');
+                $.each(
+                //for each environment in the reporting area
+                    area.Environments,
+                    function (i2, env2) {
+                        //write the name and connect to the webservice to get all the data
+                        $('#output').append('<h3>' + env2.name + ' [VC:' + env2.VCServer + ']</h3>');
 
+                        $('#output').append('<div id="data_' + env2.VCServer + '"></div><div id="comments_' + env2.VCServer + '"></div>');
+                        GetPostsIntoTable(env2.VCServer, "data_" + env2.VCServer, startDate, endDate);
+                    }
+                );
+            }
         );
+
+
+
+        // 
 
     });
 
 });
 
 
-function GetPosts(env, start, end) {
 
-    var LogRequest = {}
-
-    LogRequest.startDate = start;
-    LogRequest.endDate = end;
-    if (!env) {
-        LogRequest.environment = "prodpb2";
-    } else {
-        LogRequest.environment = env;
-    }
-
-    var DTO = { 'LogRequest': LogRequest };
-
-    jQuery("table_" + env).jqGrid({
-        url: "Service.asmx/GetEntries",
-        datatype: "json",
-        colNames:['Time','Entry','Host'],
-        colModel:[
-            {name:'Time',index:'Time'},
-            {name:'Event',index:'Event'}
-        ],
-        rowNum:10, 
-        rowList:[10,20,30], 
-        pager: "#pager_" + env, 
-        sortname: 'Time',
-        viewrecords: true,
-        sortorder: "Time", 
-        caption: "JSON Example"
-
-    });
-    jQuery("table_" + env).jqGrid('navGrid', "#pager_" + env, { edit: false, add: false, del: false });
-}
-
-
-
-function getPostsIntoTable(env,start,end) {
+function GetPostsIntoTable(env,location,start,end) {
 
     var LogRequest = {}
 
@@ -129,25 +145,21 @@ function getPostsIntoTable(env,start,end) {
             if (les.length != 0) {
 
                 //Create the table header
-                $('#output').append('<table id="table_'+ env +'" class="logdata"></table');
+                $('#' + location).append('<table id="table_' + env + '" class="display"></table');
 
                 //create the table heading row
-                $('#table_' +env).append('<thead><tr><th colspan="6" id="' + env + '">'+env+'</th></tr><tr>' +
+                $('#table_' + env).append('<thead><tr><!--<th colspan="6" id="' + env + '">' + env + '</th>--></tr><tr>' +
                 '   <td>Time</td>' +
                 '   <td>Event</td>' +
                 '   <td>#</td>' +
                 '   <td>Host</td>' +
-                '   <td>Cluster</td>' + 
+                '   <td>Cluster</td>' +
                 //'   <td> Id</td>' +
                 '   <td>InvestigationID</td>' +
 
                 '</tr></thead><tbody>');
 
                 //render the data
-                
-
-
-
 
                 $.each(les, function (index, le) {
                     $('#table_' + env).append('<tr>' +
@@ -160,16 +172,22 @@ function getPostsIntoTable(env,start,end) {
                     );
                 });
 
-                //close the table
+                //close the table body
                 $('#table_' + env).append("</tbody>");
 
-            } else {
-                $('#output').append("<p>No Data</p>");
-            }
+                $('#table_' + env).dataTable({
+                    "bPaginate": false,
+                    "bJQueryUI": true
+                });
 
-        },
-        failure: function (msg) {
-            $('#output').text(msg);
+        } else {
+            $('#' + location).append("<p>No Data</p>");
         }
-    });
+
+    },
+    failure: function (msg) {
+        $('#' + location).text(msg);
+    }
+});
 }
+
